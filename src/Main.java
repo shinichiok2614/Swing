bdd04236f86dab4503730e8faaa5e8383d9502b3
PCTUAN <phuonghoangit2614@gmail.com>
2022-11-07 08:20:57 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JFrame j=new JFrame();
+        j.setSize(300,200);
+        j.setVisible(true);
+
+    }
+}
